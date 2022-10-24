7932d728faa0a5311bffd60a68d0f720e2b2ffe1
Lucivaldo Quiroz <lucivaldo.queiroz@gmail.com>
2022-10-23 22:46:47 -0300

    Finalizacao do projeto

---
+++ Alura Ponto/Home/Model/Camera.swift
@@ -0,0 +1,50 @@
+//
+//  Camera.swift
+//  Alura Ponto
+//
+//  Created by Lucivaldo Queiroz on 23/10/22.
+//
+
+import Foundation
+import UIKit
+
+protocol CameraDelegate: AnyObject {
+    func didSelectFoto(_ image: UIImage)
+}
+
+class Camera: NSObject {
+    
+    weak var delegate: CameraDelegate?
+    
+    
+    func abrirCamera(_ controller: UIViewController, _ imagePicker: UIImagePickerController) {
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        imagePicker.cameraDevice = UIImagePickerController.isCameraDeviceAvailable(.front) ? .front : .rear
+        
+        
+        controller.present(imagePicker, animated: true)
+    }
+    
+    func abrirBibliotecaDeFotos(_ controller: UIViewController, _ imagePicker: UIImagePickerController) {
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        controller.present(imagePicker, animated: true)
+    }
+}
+
+extension Camera: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true)
+        
+        guard let foto = info[.editedImage] as? UIImage else { return }
+        
+        delegate?.didSelectFoto(foto)
+    }
+}
